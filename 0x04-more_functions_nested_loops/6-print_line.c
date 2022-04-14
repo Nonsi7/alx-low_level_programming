e046eadad0b116978ef2dae3d96707040d2e5e2c
Nonsi7 <debockannaline@gmail.com>
2022-04-14 13:31:52 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - prints underscores
+ * @n: limit
+ *
+ * Return: void
+ */
+
+void print_line(void)
+{
+int i;
+
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
