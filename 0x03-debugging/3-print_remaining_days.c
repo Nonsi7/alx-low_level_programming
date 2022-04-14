3871605bfcc626059b4ce08bb014ba6c1ecf3ff4
Nonsi7 <debockannaline@gmail.com>
2022-04-14 08:29:18 -0700

    Task3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,36 +11,34 @@
  */
 
 void print_remaining_days(int month, int day, int year)
-{
-<<<<<<< HEAD
+{  
+
         if (year % 4 == 0 || ((year % 400 == 0) && !(year % 100 == 0)))
-        {
+        { 
                 if (month > 2 && day >= 60)
         {         
                 day++;
         }
-=======
-  if (year % 100 == 0 || ((year % 400 == 0) && !(year % 4 == 0)))
-  {
-  if (month > 2 && day >= 60)
-  {        
-  day++;
-  }
->>>>>>> 1afca468213357623b15dd1e06e1000f331b4438
-
+                if (year % 100 == 0 || ((year % 400 == 0) && !(year % 4 == 0)))
+        {
+                if (month > 2 && day >= 60)
+        {        
+                day++;
+        }
+                        
                 printf("Day of the year: %d\n", day);
                 printf("Remaining days: %d\n", 366 - day);
-        }
+        }  
         else
-        {     
+        {      
                 if (month == 2 && day == 60)
-        {
+	{ 
                 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
         }
         else
         {
                 printf("Day of the year: %d\n", day);
                 printf("Remaining days: %d\n", 365 - day);
-        }  
+	}  
         }
 }
