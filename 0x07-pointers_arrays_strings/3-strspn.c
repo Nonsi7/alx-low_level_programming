9d71549a5c6b2fb6c6ae619301693ac74dcd156b
Nonsi7 <debockannaline@gmail.com>
2022-04-25 05:41:27 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,6 +4,7 @@
  * _strspn - gets the length of a prefix substring
  * @s: input string to search for substring
  * @accept: characters that prefix substring must include
+ *
  * Return: length of prefix vsubstring
  */
 
