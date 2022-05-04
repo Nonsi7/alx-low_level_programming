bd9e7476e81dc7e7efe11fda861eab0599b7307c
Nonsi7 <debockannaline@gmail.com>
2022-05-04 09:34:10 -0700

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -6,6 +6,8 @@
  * alloc_grid - returns a pointer to a 2 dimensional array of integers
  * @width: columns
  * @height: rows
+ *
+ * Return: Double Pointer
  */
 
 int **alloc_grid(int width, int height)
