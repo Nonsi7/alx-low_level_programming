ea830d9ef5fc53eaafe037af7b41286f2d81e815
Nonsi <debockannaline@gmail.com>
2022-05-07 15:54:57 +0200

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,7 +5,7 @@
  * _atoi_digit - convert a char to integer.
  * @x: character to convert.
  * Return: integer.
- */
+ **/
 
 int _atoi_digit(char x)
 {
@@ -20,8 +20,7 @@ int _atoi_digit(char x)
  * _isNumber - Define if a string is a number.
  * @argv: Pointer to string.
  * Return: success (0).
- */
-
+ **/
 int _isNumber(char *argv)
 {
 	int i;
@@ -33,11 +32,11 @@ int _isNumber(char *argv)
 }
 
 /**
- * _calloc - allocate array of size * nmemb.
+ *_calloc - allocate array of size * nmemb.
  * @nmemb: number of elements.
  * @size: size of element.
  * Return: pointer to array.
- */
+ **/
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
@@ -63,13 +62,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
  * @a3: array for result.
  * @lena: length of array a3.
  * Return: pointer to array.
- */
+ **/
 
 void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
 {
 	int mul = 0, i, k;
 
-	k = lena;for (i = len1 - 1; i >= 0 ; i--)
+	k = lena;
+	for (i = len1 - 1; i >= 0 ; i--)
 	{
 		mul += (a1[i] - '0') * (a2 - '0') + (a3[k] - '0');
 		a3[k] = (mul % 10) + '0';
@@ -77,13 +77,13 @@ void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
 		k--;
 	}
 
-	while (mul != 0)
-	{
-		mul += a3[k] - '0';
-		a3[k] = (mul % 10) + '0';
-		mul /= 10;
-		k--;
-	}
+		while (mul != 0)
+		{
+			mul += a3[k] - '0';
+			a3[k] = (mul % 10) + '0';
+			mul /= 10;
+			k--;
+		}
 
 	return (a3);
 }
@@ -92,7 +92,6 @@ void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
  * @nb: number of elements to print.
  * @a: array of elements.
  **/
-
 void print_array(char *a, int nb)
 {
 	int i = 0;
@@ -109,10 +108,10 @@ void print_array(char *a, int nb)
 }
 
 /**
- * main - print the multiplication of 2 numbers.
- * @argc: array length.
- * @argv: array.
- * Return: 0.
+ *main - print the multiplication of 2 numbers.
+ *@argc: array length.
+ *@argv: array.
+ *Return: 0.
  */
 
 int main(int argc, char *argv[])
@@ -130,21 +129,20 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	for (len1 = 0; argv[1][len1]; len1++)
-		;
+	;
 	for (len2 = 0; argv[2][len2]; len2++)
-		;
+	;
 	lenres = len1 + len2;
 	tabres = _calloc(lenres, sizeof(int));
 	if (tabres == NULL)
 	{
 		free(tabres);
 		return (0);
-
 	}
 	for (i = len2 - 1, c = 0; i >= 0; i--)
 	{
-		tabres = mul_array(argv[1], len1, argv[2][i], tabres, (lenres - 1 - c));
-		c++;
+	tabres = mul_array(argv[1], len1, argv[2][i], tabres, (lenres - 1 - c));
+	c++;
 	}
 	print_array(tabres, lenres);
 	free(tabres);
