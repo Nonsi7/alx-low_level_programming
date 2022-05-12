59b2dea70ca8385443d3161414408b67ac6a784a
Nonsi7 <debockannaline@gmail.com>
2022-05-12 08:19:20 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -10,7 +10,7 @@
  * @name: name to be printed
  * @f: callback function that prints name
  */
-void print_name(char *name, void (*f)(char *))
+int void print_name(char *name, void (*f)(char *))
 {
 	if (!f || !name)
 		return;
